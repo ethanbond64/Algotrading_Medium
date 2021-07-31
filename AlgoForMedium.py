64583a5a152c5182353985b1d211ee1a331cb331
Pylons <ngedwards998@gmail.com>
2021-07-31 15:12:19 -0700

    fixes the price variable error
    coinbase API returns 'None" when the currency variable is set to 'DOGE-USD'
    but returns the ID when you just put 'DOGE'

--- AlgoForMedium.py
+++ AlgoForMedium.py
@@ -1,29 +1,30 @@
+import cbpro
 import numpy as np
 import datetime as dt
-import cbpro
 import time
-################################################################################
-                             ## API STUFF ###
 
-# CB Pro granted api credentials as strings
+public_client = cbpro.PublicClient()
+
+
+
 apiKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 apiSecret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 passphrase = "XXXXXXXXXXX"
 
 auth_client = cbpro.AuthenticatedClient(apiKey,apiSecret,passphrase)
 
-################################################################################
                         ### Investment Details ###
 
 # Amount to initially invest
-initInvestment = 20.00
+initInvestment = 20.0
 
 # Amount that will be used for purchase starts at the initial amount
 funding = initInvestment
 
 # Currency to trade, for reference:
 # 'BCH-USD' = Bitcoin Cash, 'BTC-USD' = Bitcoin, 'ETH-USD' = Ether
-currency = 'BCH-USD'
+currency = 'DOGE-USD'
+specific_currency = 'DOGE'
 
 # Will return the ID of your specific currency account
 def getSpecificAccount(cur):
@@ -33,10 +34,11 @@ def getSpecificAccount(cur):
             return account['id']
 
 # Get the currency's specific ID
-specificID = getSpecificAccount(currency[:3])
 
-# Granularity (in seconds). So 300 = data from every 5 min
-period = 300
+specificID = getSpecificAccount(str(specific_currency))
+
+# Granularity (in seconds). So 300 = data from every 5 min (its a stickler about the seconds tho)
+period = 500
 
 # We will keep track of how many iterations our bot has done
 iteration = 1
@@ -44,31 +46,24 @@ iteration = 1
 # Start off by looking to buy
 buy = True
 
-################################################################################
-                ### Begin Loop and get Historic Data ###
-
 while True:
-
     try:
-        historicData = auth_client.get_product_historic_rates(currency, granularity=Period)
+        historicData = auth_client.get_product_historic_rates(currency, granularity=period)
 
         # Make an array of the historic price data from the matrix
         price = np.squeeze(np.asarray(np.matrix(historicData)[:,4]))
-
+        
         # Wait for 1 second, to avoid API limit
         time.sleep(1)
 
         # Get latest data and show to the user for reference
         newData = auth_client.get_product_ticker(product_id=currency)
         print(newData)
-        currentPrice=newData['price']
-
+        currentPrice = newData['price']
     except:
         # In case something went wrong with cbpro
         print("Error Encountered")
-
-################################################################################
-                            ### Strategy ###
+        break
 
     # Calculate the rate of change 11 and 14 units back, then sum them
     ROC11 = np.zeros(13)
@@ -80,8 +75,8 @@ while True:
         ROC14[ii] = (100*(price[ii]-price[ii+14]) / float(price[ii+14]))
         ROCSUM[ii] = ( ROC11[ii] + ROC14[ii] )
 
-    # Calculate the past 4 Coppock values with Weighted Moving Average
     coppock = np.zeros(4)
+    # Calculate the past 4 Coppock values with Weighted Moving Average
     for ll in range(0,4):
         coppock[ll] = (((1*ROCSUM[ll+9]) + (2*ROCSUM[ll+8]) + (3*ROCSUM[ll+7]) \
         + (4*ROCSUM[ll+6]) + (5*ROCSUM[ll+5]) + (6*ROCSUM[ll+4]) \
@@ -90,14 +85,13 @@ while True:
 
     # Calculate the past 3 derivatives of the Coppock Curve
     coppockD1 = np.zeros(3)
-    for mm in range(3):
+    for mm in range(0,3):
         coppockD1[mm] = coppock[mm] - coppock[mm+1]
-
-################################################################################
-                            ### Funds to Use ###
 
     # The maximum amount of Cryptocurrency that can be purchased with your funds
     possiblePurchase = (float(funding)) / float(currentPrice)
+
+    print(specificID)
 
     # The amount of currency owned
     owned = float(auth_client.get_account(specificID)['available'])
@@ -105,11 +99,8 @@ while True:
     # The value of the cryptourrency in USD
     possibleIncome = float(currentPrice) * owned
 
-################################################################################
-                             ###Decision Making###
-
     # Buy Conditions: latest derivative is + and previous is -
-    if buy == True and (coppockD1[0]/abs(coppockD1[0])) == 1.0 and (coppockD1[1]/abs(coppockD1[1])) == -1.0:
+    if buy == True and (coppockD1[0]/abs(coppockD1[0])) == 1 and (coppockD1[1]/abs(coppockD1[1])) == -1:
 
         # Place the order
         auth_client.place_market_order(product_id=currency, side='buy', funds=str(funding))
@@ -124,7 +115,7 @@ while True:
         buy = False
 
     # Sell Conditions: latest derivative is - and previous is +
-    if buy == False and (coppockD1[0]/abs(coppockD1[0])) == -1.0 and (coppockD1[1]/abs(coppockD1[1])) == 1.0:
+    if buy == False and (coppockD1[0]/abs(coppockD1[0])) == -1 and (coppockD1[1]/abs(coppockD1[1])) == 1:
 
         # Place the order
         auth_client.place_market_order(product_id=currency,side='sell',size=str(owned))
@@ -143,7 +134,7 @@ while True:
 
         # If there is any of the crypto owned, sell it all
         if owned > 0.0:
-            auth_client.place_market_order(product_id = currency, side='sell', size = str(owned))
+            auth_client.place_market_order(product_id=currency, side='sell', size=str(owned))
             print("STOP LOSS SOLD ALL")
 
         # Will break out of the while loop and the program will end
@@ -156,8 +147,8 @@ while True:
     # Print the details for reference
     print("Current Price: ", currentPrice)
     print("Your Funds = ", funding)
-    print("You Own ", owned, "BCH")
+    print("You Own ", owned, currency)
 
-    # Wait for 5 minutes before repeating
+    # Wait for however long the period variable is before repeating
     time.sleep(300)
     iteration += 1
