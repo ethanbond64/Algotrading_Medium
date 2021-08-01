fc08313d40ffcf7bf4eae459902a1d2ec7918aae
ngedwards998 <ngedwards998@gmail.com>
2021-08-01 12:46:43 -0700

    added a message at the end that shows how much of your initInvestment variable is invested

--- BTC-ADA-ETH-DOGE.py
+++ BTC-ADA-ETH-DOGE.py
@@ -296,7 +296,7 @@ while True:
     print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
     stats(currentPrice3, funding3, owned3, currency3, statscoppock3)
     print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-    print('total : $' + str(initInvestment))
+    print('Total invested : $' + str(abs(int(funding + funding1 + funding2 + funding3) - int(initInvestment))))
     # Wait for however long the period variable is before repeating
     time.sleep(period)
     iteration += 1
